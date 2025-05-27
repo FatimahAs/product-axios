da99c079ae117377e69b458c54efeb3e3d98c949
FatimahAs <46461256+FatimahAs@users.noreply.github.com>
2025-05-27 11:36:32 +0300

    edit

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -15,7 +15,7 @@ function Products() {
     <div className="min-h-screen bg-white p-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map(product => (
-          <div key={product.id} className="bg-white shadow-md rounded-2xl p-4 hover:shadow-lg transition duration-300 border-[#0c522a] border-1">
+          <div className="bg-white shadow-md rounded-2xl p-4 hover:shadow-lg transition duration-300 border-[#0c522a] border-1">
             <img src={product.image} alt={product.title} className="h-40 mx-auto mb-4 object-contain " />
             
             <h2 className="text-md font-semibold text-gray-700">{product.title}</h2>
