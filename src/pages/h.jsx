99ae45e8b090d5b31453ddddbecd211637bc9203
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-05-27 11:11:46 +0300

    edit icon

--- src/pages/h.jsx
+++ src/pages/h.jsx
@@ -1,43 +1,43 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
+//import { useEffect, useState } from 'react'
+//import axios from 'axios';
 
-import './App.css'
+//import './App.css'
 
-function App() {
-  const [info, setInfo] = useState('posts');
-  const [item, setItem] = useState([]);
-  useEffect(() => {
-    axios.get('https:').then((res) => { setItem(res.data) })
-      //.then((response) => response.json())
-    //.then((data) => setItem(data))
+//function App() {
+//  const [info, setInfo] = useState('posts');
+//  const [item, setItem] = useState([]);
+//  useEffect(() => {
+//    axios.get('https:').then((res) => { setItem(res.data) })
+//      //.then((response) => response.json())
+//    //.then((data) => setItem(data))
 
-  },[])
+//  },[])
 
-// 3type
-  return (
-    <>
-      <p>{info}</p>
-      {item.map(items => {
-        return (
-          <>
-            <li>{items}</li>
-          </>
-        )
-      })}
-      <button onClick={() => {
-        (setInfo('posts'))
-      }}
-      >post</button>
-        <button onClick={() => {
-        (setInfo('albums'))
-      }}
-      >albums</button>
-        <button onClick={() => {
-        (setInfo('users'))
-      }}
-      >users</button>
-    </>
-  )
-}
+//// 3type
+//  return (
+//    <>
+//      <p>{info}</p>
+//      {item.map(items => {
+//        return (
+//          <>
+//            <li>{items}</li>
+//          </>
+//        )
+//      })}
+//      <button onClick={() => {
+//        (setInfo('posts'))
+//      }}
+//      >post</button>
+//        <button onClick={() => {
+//        (setInfo('albums'))
+//      }}
+//      >albums</button>
+//        <button onClick={() => {
+//        (setInfo('users'))
+//      }}
+//      >users</button>
+//    </>
+//  )
+//}
 
-export default App
+//export default App
